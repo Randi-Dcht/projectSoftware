72d16361f19e476f0b82a62044959543855e8d20
Randi <dev@dochot.be>
2023-04-25 23:31:24 +0200

    git add test for convert infix

--- src/test/java/junit5tests/TestConvertNotation.java
+++ src/test/java/junit5tests/TestConvertNotation.java
@@ -41,4 +41,23 @@ class TestConvertNotation
         assertEquals(listing.get(5).getValue(), "*");
         assertEquals(listing.get(6).getValue(), "+");
     }
+
+    @Test
+    void testConvertNotation_3_infix()
+    {
+        String s = "1 + 2 * 3 - 2 * 1";
+        List<Typos> list = StringRegrex.analyse(s);
+        List<Typos> listing = transformNotation(Notation.INFIX, list, false);
+
+        assertEquals(listing.size(), 9);
+        assertEquals(listing.get(0).getValue(), "1");
+        assertEquals(listing.get(1).getValue(), "2");
+        assertEquals(listing.get(2).getValue(), "3");
+        assertEquals(listing.get(3).getValue(), "*");
+        assertEquals(listing.get(4).getValue(), "+");
+        assertEquals(listing.get(5).getValue(), "2");
+        assertEquals(listing.get(6).getValue(), "1");
+        assertEquals(listing.get(7).getValue(), "*");
+        assertEquals(listing.get(8).getValue(), "-");
+    }
 }
